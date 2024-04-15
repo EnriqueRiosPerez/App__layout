c72f38981c1e6c6cebcd827b6636d5a9cb6fbca3
Enrique Casa <enriquemanuelriosperez@gmail.com>
2024-04-14 22:26:22 -0600

    funciones del aside modificadas

--- NavPropio/NewVersion_interactive.js
+++ NavPropio/NewVersion_interactive.js
@@ -1,18 +1,18 @@
+
+
+
 let subMenusHTML = document.getElementsByClassName("Menu__Item__Submenu")
 let Items = document.getElementsByClassName("Menu__Item__Title")
 let usuarioImageInfo = document.getElementById("subMenu")
 let rowsIcons = document.getElementsByClassName("Menu__Item__Title__sub")
-// let side_nav = document.getElementById("Ul_Menu")
-// let side_nav_Li_Elements = side_nav.querySelectorAll("li.Menu__Item")
-// side_nav_Li_Elements.forEach(li=>{
-//     li.addEventListener("click", imprimmir_Elemento);
-// })
 
 
 export async function ShowItemNav(e) {
     let current = e.target
     console.log(current)
+    //estos son los elemntos que son seleccionados
     let title = current.querySelectorAll("span.Menu__Item__Title")
+
     let submenu = current.querySelectorAll("ul.Menu__Item__Submenu")
     let rowIcon = current.querySelectorAll("span.Menu__Item__Title__sub")
 
@@ -22,85 +22,65 @@ export async function ShowItemNav(e) {
     rotateRowIcon(rowIcon[0])
     console.timeEnd("Ocultar2")
 }
-
-
-
 function rotateRowIcon(current) {
     return new Promise((resolve, reject) => {
         let iconRows = [...rowsIcons]
         let iconRows_length = iconRows.length
-        if(iconRows_length === 0 ){
+        if (iconRows_length === 0) {
             reject(new Error("No se tienen rows"))
             return
         }
-        // for(let i = 0; i < iconRows_length; i++){
-        //     iconRows[i].classList.toggle("c_rotateIcon", element === current)
-        // }
         iconRows.forEach(element => {
-            //se mejoro la logica para hacer los cambios  
+            
             element !== current ? element.classList.remove("c_rotateIcon") : element.classList.toggle("c_rotateIcon");
-           //se revisa la logia y se observa que es mas facil utilizar el toggle para poder hacer los cambios    !!Falta revisar la velocidad con la que realiza 
-            //el segundo es el valor condicional de hacer el toggle en este caso tiene que ser 
-            // element.classList.toggle("c_rotateIcon", element === current)
+           
         })
         resolve("yeah rotate!")
     })
 }
 
 
+function hideNavSubmenusPromise(current) {
+    return new Promise((resolve, reject) => {
+        if (!current) {reject(new Error("Submenu element not found"))
+         return
+        }
+        let subMenus = [...subMenusHTML]
+        subMenus.forEach(element => {
+            if (element !== current) {
+                element.classList.remove("open")
+            } else {
+                element.classList.toggle("open")
+            }
+        })
+        resolve("Okay");
+    })
+}
+
 function selectMenuPromise(current) {
     return new Promise((resolve, reject) => {
         let items = [...Items]
-        let si = []
-        let no = []
-        if (items.length === 0) {
-            reject(new Error("No se tiene items"))
+        if (!current) {
+            reject(new Error("Element no select"))
         }
         items.forEach(element => {
             if (element !== current) {
-                no.push(element)
                 element.classList.remove("seleccionando")
             } else {
-                si.push(element)
                 element.classList.toggle("seleccionando")
-            }
-        });
-        resolve("yeah!")
-    })
-
-}
-
-function hideNavSubmenusPromise(current) {
-    return new Promise((resolve, reject) => {
-        let subMenus = [...subMenusHTML]
-        let si = []
-        let no = []
-        if (subMenus.length === 0) {
-            reject(new console.Error("none"))
-        }
-        subMenus.forEach(element => {
-            if (element !== current) {
-                no.push(element)
-                element.classList.remove("open")
-            } else {
-                si.push(element)
-                element.classList.toggle("open")
             }
         });
         resolve("Okay")
     })
-
 }
 
-
 export async function revisarClase(e) {
-    return new Promise((resoove, reject) => {
+    return new Promise((resolve, reject) => {
         let current = e.target
         if (usuarioImageInfo.classList.contains("mostrar")) {
             if (current.id === "subMenu") {
                 usuarioImageInfo.classList.remove("mostrar")
             }
-
         }
         else {
             usuarioImageInfo.classList.add("mostrar")
