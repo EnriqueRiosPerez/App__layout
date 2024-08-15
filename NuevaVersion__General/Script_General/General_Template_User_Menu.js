5ac2bc6a21c9d76e9594eec5c95330d7f93d1eaf
gdjenrio <enriquemanuelriosperez@gmail.com>
2024-08-15 10:47:38 -0600

    Se terminar la version preliminar

--- NuevaVersion__General/Script_General/General_Template_User_Menu.js
+++ NuevaVersion__General/Script_General/General_Template_User_Menu.js
@@ -1,65 +1,53 @@
 ﻿
-export const Template__User_Menu__HTLM = `<div class="item_info">
-                <div class="item_infoImg">
-                 
+export const Template__User_Menu__HTLM = `
+            <header class="item_info">
+                <div>
                     <image id="headIamgen_IMG2323" class="iconoUsuario" alt="imagen de usuario" loading="lazy" src="https://gdjnt238.americas.ad.flextronics.com/flexmdc/serv1/ccl_pagina_gaffette_getImg.ashx?e=1&id=1025007" class="avatar_Aviso"></image>
-                   
                 </div>
-                <div class="item_infoText_primaria">
-                    <div class="containder2">
-                        <span id="HeadInf_Nombre" class="camposKey"> gdjenrio</span>
-                        
+                <div>
+                    <div>
+                        <span id="HeadInf_Nombre" class="camposKey"> gdjenrio</span>    
                     </div>
-                    <div class="containder2">
-                        <span id="HeadInf_Puesto" class="camposValue">Technican</span>
-                        
+                    <div>
+                        <span id="HeadInf_Puesto" class="camposValue">Technican</span>    
                     </div>
                 </div>
-            </div>
+            </header>
             <div class="item_infoText_secundaria">
-                <div class="containder2">
+                <div>
                     <span class="camposKey">Department:</span>
                     <span id="HeadInf_Depto" class="camposValue">Maintenance Preventive</span>
                     
                 </div>
-                <div class="containder2">
+                <div>
                     <span class="camposKey">Building:</span>
                     <span id="HeadInf_Nave" class="camposValue">No read</span>
                     
                 </div>
             </div>
             <section class="item_opc">
-                <span class="navLinksItems_item_opc">
-                    <span class="navLinksItems_A_icons_item_opc">
-                        <i class="fa-solid fa-user"></i>
-   
-                    </span>
-                    <a class="navLinksItems_A_Letter_item_opc" href="../Configuration/Administrator_User.aspx">User</a>
-                    
-               
+                <span>
+                    <a href="../Configuration/Administrator_User.aspx">
+                        <i class="fa-solid fa-user"></i>User
+                    </a>
                 </span>
-                <span class="navLinksItems_item_opc">
-                    <span class="navLinksItems_A_icons_item_opc">
-                        <i class="fa-solid fa-location-dot"></i>
-   
-                    </span>
-                    <span class="navLinksItems_A_Letter_item_opc">Add Location</span>
+                <span>
+                    <a href="../Configuration/Administrator_User.aspx">
+                       <i class="fa-solid fa-location-dot"></i>Add Location
+                    </a>
                 </span>
-                <span class="navLinksItems_item_opc">
-                    <span class="navLinksItems_A_icons_item_opc">
-                        <i class="fa-solid fa-gear"></i>
-   
-                    </span>
-   
-                    <span class="navLinksItems_A_Letter_item_opc">Add Equipment</span>
+                 <span>
+                    <a href="../Configuration/Administrator_User.aspx">
+                       <i class="fa-solid fa-gear"></i>Add Equipment
+                    </a>
                 </span>
-                <span class="navLinksItems_item_opc">
-                    <span class="navLinksItems_A_icons_item_opc">
-   
-                        <i class="fa-solid fa-chart-simple"></i>
-                    </span>
-                    <span class="navLinksItems_A_Letter_item_opc">Skill Settings</span>
+                <span>
+                    <a href="../Configuration/Administrator_User.aspx">
+                       <i class="fa-solid fa-chart-simple"></i>Skill Settings
+                    </a>
                 </span>
+
+                
             </section>
              <footer class="item_button">
                 <a href="../../Login.aspx" class="item_button_button" style="display: flex;
